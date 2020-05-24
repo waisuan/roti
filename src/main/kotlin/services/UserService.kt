1f642a74cc70a69c6627c3e7f3524de771772c69
Evan Sia <ws-91@hotmail.com>
2020-05-24 23:13:38 +0100

    Fix linter issues

--- src/main/kotlin/services/UserService.kt
+++ src/main/kotlin/services/UserService.kt
@@ -3,7 +3,6 @@ package services
 import models.User
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.mindrot.jbcrypt.BCrypt
 import tables.UserTable

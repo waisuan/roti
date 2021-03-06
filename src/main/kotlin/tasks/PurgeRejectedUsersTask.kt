67b8fafc4b60937fb7f232b1f4e0c4f411e3a5ec
Evan Sia <ws-91@hotmail.com>
2021-03-06 22:29:27 +0000

    Use println for task instead of logger

--- src/main/kotlin/tasks/PurgeRejectedUsersTask.kt
+++ src/main/kotlin/tasks/PurgeRejectedUsersTask.kt
@@ -2,13 +2,12 @@ package tasks
 
 import picocli.CommandLine
 import services.UserService
-import utils.logger
 
 @CommandLine.Command(name = "purge_rejected_users", mixinStandardHelpOptions = true, description = ["delete rejected users from DB"])
 class PurgeRejectedUsersTask : Task {
     override fun call(): Int {
         UserService.getRejectedUsers().forEach {
-            logger().info("Deleting: $it")
+            println("Deleting: $it")
             UserService.deleteUser(it.username!!)
         }
         return 0

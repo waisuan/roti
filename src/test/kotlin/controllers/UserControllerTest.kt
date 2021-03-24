83c967439c35de3ea2b032e335fe56b4579743ee
Evan Sia <ws-91@hotmail.com>
2021-03-24 23:12:44 +0000

    Update javalin to 4.0.0-ALPHA

--- src/test/kotlin/controllers/UserControllerTest.kt
+++ src/test/kotlin/controllers/UserControllerTest.kt
@@ -1,21 +1,18 @@
 package controllers
 
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.eq
-import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
-import com.nhaarman.mockitokotlin2.whenever
 import exceptions.IllegalUserException
 import helpers.TestDatabase
 import io.javalin.http.Context
+import io.javalin.http.context.body
+import io.mockk.every
+import io.mockk.mockk
 import models.Constants
 import models.User
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito
 import services.UserService
 
 class UserControllerTest {
@@ -26,16 +23,15 @@ class UserControllerTest {
 
     @AfterEach
     fun tearDown() {
-        Mockito.framework().clearInlineMocks()
         TestDatabase.purge()
     }
 
     @Test
     fun `loginUser() throws an exception if unsuccessful`() {
-        val context = mock<Context>()
+        val context = mockk<Context>(relaxed = true)
         val user = User(username = "TEST", password = "TEST")
 
-        whenever(context.body<User>()).thenReturn(user)
+        every { context.body<User>() } returns user
 
         assertThatThrownBy {
             UserController.loginUser(context)
@@ -44,33 +40,33 @@ class UserControllerTest {
 
     @Test
     fun `loginUser() writes to cookie`() {
-        val context = mock<Context>()
+        val context = mockk<Context>(relaxed = true)
         val user = User(username = "TEST", password = "TEST", email = "test@mail.com")
         UserService.createUser(user)
         UserService.approveUser(user.username!!)
 
-        whenever(context.body<User>()).thenReturn(user)
-        whenever(context.host()).doReturn("localhost")
+        every { context.body<User>() } returns user
+        every { context.url() } returns "http://localhost"
 
         UserController.loginUser(context)
 
-        verify(context).cookie(eq(Constants.USER_TOKEN.name), any(), any())
-        verify(context).cookie(eq(Constants.USER_NAME.name), eq(user.username!!), any())
-        verify(context).json(any<User>())
+        verify { context.cookie(Constants.USER_TOKEN.name, any()) }
+        verify { context.cookie(Constants.USER_NAME.name, user.username!!) }
+        verify { context.json(any<User>()) }
     }
 
     @Test
     fun `logoutUser() removes cookies`() {
-        val context = mock<Context>()
+        val context = mockk<Context>(relaxed = true)
 
-        whenever(context.host()).doReturn("localhost")
+        every { context.url() } returns "http://localhost"
 
         context.cookie(Constants.USER_TOKEN.name, "TOKEN")
         context.cookie(Constants.USER_NAME.name, "USER")
 
         UserController.logoutUser(context)
 
-        verify(context).removeCookie(eq(Constants.USER_TOKEN.name), eq("/"))
-        verify(context).removeCookie(eq(Constants.USER_NAME.name), eq("/"))
+        verify { context.removeCookie(Constants.USER_TOKEN.name, "/") }
+        verify { context.removeCookie(Constants.USER_NAME.name, "/") }
     }
 }
